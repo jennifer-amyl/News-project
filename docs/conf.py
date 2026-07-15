184d3851763e76557c33f29ea7501d092fa7971e
jennifer-amyl <jennifer.f16@icloud.com>
2026-07-15 13:02:00 +0100

    Fix Sphinx documentation structure

--- docs/conf.py
+++ docs/conf.py
@@ -11,6 +11,8 @@ import sys
 import django
 
 sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../news'))
+sys.path.insert(0, os.path.abspath('../news_project'))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'news_project.settings'
 django.setup()
 
@@ -22,7 +24,9 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
